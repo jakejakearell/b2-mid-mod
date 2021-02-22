173f0665b5785928924da395c171a70649257783
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-22 11:57:44 -0700

    Final commits

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -10,9 +10,9 @@ class MechanicsController < ApplicationController
   def new
     @mechanic = Mechanic.find(params[:mechanic_id])
     ride = Ride.find(params[:ride_id])
-    new_job = RideMechanic.new(ride: ride, mechanic: @mechanic)
+    RideMechanic.create!(ride: ride, mechanic: @mechanic)
 
     redirect_to "/mechanics/#{@mechanic.id}"
-    
+
   end
 end

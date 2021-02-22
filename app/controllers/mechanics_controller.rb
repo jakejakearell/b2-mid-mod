10455ad0ecd214b004a18aaba55fe2cc6ca0a66d
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-22 11:51:36 -0700

    Close end spaces

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -6,4 +6,13 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:id])
   end
+
+  def new
+    @mechanic = Mechanic.find(params[:mechanic_id])
+    ride = Ride.find(params[:ride_id])
+    new_job = RideMechanic.new(ride: ride, mechanic: @mechanic)
+
+    redirect_to "/mechanics/#{@mechanic.id}"
+    
+  end
 end

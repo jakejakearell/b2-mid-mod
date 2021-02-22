3d547921f20a542ee4b9862544ad40e04b228d06
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-22 10:43:15 -0700

    Add code to pass user story 2

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,8 @@
 class Ride < ApplicationRecord
   has_many :ride_mechanics
   has_many :mechanics, through: :ride_mechanics
+
+  def self.open_and_most_thrilling
+    where("open = ?", true).order(thrill_rating: :desc)
+  end
 end
